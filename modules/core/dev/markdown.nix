f359a8b5fd7affc684461f2da208e5df4388bc22
Jonathan Andersson <zeapo@pm.me>
2026-01-24 14:32:49 +0700

    Move programming languages into separate modules

--- modules/core/dev/markdown.nix
+++ modules/core/dev/markdown.nix
@@ -1,18 +1,9 @@
 {
   flake.modules.nixos.core = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
-      # Nix.
-      nil
-      alejandra
-      statix
-      # Rust.
-      rustup
-      # Markdown.
       marksman
       markdownlint-cli2
       prettier
-      # Lua.
-      lua-language-server
     ];
   };
 }
