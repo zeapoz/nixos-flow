5afe9a4a91db9b122b36698deb5050bd82c0bb65
Jonathan Andersson <zeapo@pm.me>
2026-02-08 17:38:02 +0700

    Remove nix store mirror

--- modules/core/nix/settings.nix
+++ modules/core/nix/settings.nix
@@ -1,9 +1,6 @@
 {
   flake.modules.nixos.core = {
-    nix = {
-      settings.substituters = ["https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"];
-      settings.experimental-features = ["nix-command" "flakes"];
-    };
+    nix.settings.experimental-features = ["nix-command" "flakes"];
 
     nixpkgs.config.allowUnfree = true;
   };
