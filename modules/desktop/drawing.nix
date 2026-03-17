6e672e8b56ad6235820f6c1b8da6c66019dc7a49
Jonathan Andersson <zeapo@pm.me>
2026-03-17 13:04:30 +0900

    Remove krita override

--- modules/desktop/drawing.nix
+++ modules/desktop/drawing.nix
@@ -1,16 +1,6 @@
 {
-  flake.modules.nixos.desktop = {pkgs, ...}: let
-    # Fix build failing for boost 1.89,
-    # https://github.com/NixOS/nixpkgs/issues/485826
-    boost = pkgs.boost187;
-    krita = pkgs.krita-unwrapped.override {
-      inherit boost;
-      lager = pkgs.lager.override {
-        inherit boost;
-      };
-    };
-  in {
-    environment.systemPackages = [
+  flake.modules.nixos.desktop = {pkgs, ...}: {
+    environment.systemPackages = with pkgs; [
       krita
     ];
   };
