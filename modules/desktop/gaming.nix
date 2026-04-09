8f105675eea95394459e4982ecc4967e3fc4b54c
Jonathan Andersson <zeapo@pm.me>
2026-04-09 12:33:38 +0900

    Setup wine, gamescope and retroarch

--- modules/desktop/gaming.nix
+++ modules/desktop/gaming.nix
@@ -1,5 +1,28 @@
 {
-  flake.modules.nixos.desktop = {
-    programs.steam.enable = true;
+  flake.modules.nixos.desktop = {pkgs, ...}: let
+    retroarch = pkgs.retroarch.withCores (cores:
+      with cores; [
+        mesen
+        gambatte
+      ]);
+  in {
+    environment.systemPackages = with pkgs; [
+      retroarch
+      lutris
+      wineWow64Packages.stable
+      winetricks
+    ];
+
+    programs = {
+      steam = {
+        enable = true;
+        gamescopeSession.enable = true;
+      };
+
+      gamescope = {
+        enable = true;
+        capSysNice = false;
+      };
+    };
   };
 }
