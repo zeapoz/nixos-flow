07627d95a82909155c11d32266ba6362d41d39e9
zeapoz <zeapo@pm.me>
2026-06-02 10:38:48 +0200

    Update theme toggle script

--- modules/desktop/theming/toggle-color-scheme.sh
+++ modules/desktop/theming/toggle-color-scheme.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-#
 # Toggles between dark and light mode.
 
 KEY=/org/gnome/desktop/interface/color-scheme
@@ -9,10 +8,14 @@ if [ "$current" = "'prefer-light'" ]; then
   mode=dark
   icon="dark_mode"
   dconf write "$KEY" "'prefer-dark'"
+  dconf write /org/gnome/desktop/interface/gtk-theme "'Orchis-Purple-Dark'"
+  dconf write /org/gnome/desktop/interface/icon-theme "'Papirus-Dark'"
 else
   mode=light
   icon="light_mode"
   dconf write "$KEY" "'prefer-light'"
+  dconf write /org/gnome/desktop/interface/gtk-theme "'Orchis-Purple-Light'"
+  dconf write /org/gnome/desktop/interface/icon-theme "'Papirus-Light'"
 fi
 
 TITLE="Color Scheme"
