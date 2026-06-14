2b2d203011d0c4009c1e372578b3f01c2744699e
zeapoz <zeapo@pm.me>
2026-06-14 11:51:02 +0200

    Remove zen browser

--- modules/desktop/browsers.nix
+++ modules/desktop/browsers.nix
@@ -1,8 +1,7 @@
-{inputs, ...}: {
+{
   flake.modules.nixos.desktop = {pkgs, ...}: {
     environment.systemPackages = with pkgs; [
       brave
-      inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
   };
 }
